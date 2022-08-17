126ee1dc555fd6e3775d9896df775ccd7177d2b9
haley914 <111421468+haley914@users.noreply.github.com>
2022-08-17 17:20:05 +0800

    Update button.js

--- Scripts/button.js
+++ Scripts/button.js
@@ -357,9 +357,9 @@ function App() {
                 </DragDropContext>
             </div>
             <hr />
-            <button type="button" className="btn btn-primary" onClick={teamming}>立即分隊</button>
-            <button type="button" className="btn btn-info" onClick={drawOneToBlue}>加藍隊</button>
-            <button type="button" className="btn btn-info" onClick={drawOneToRed}>加紅隊</button>
+            <button type="button" className="btn btn-primary m-2" onClick={teamming}>立即分隊</button>
+            <button type="button" className="btn btn-info m-2" onClick={drawOneToBlue}>抽一人入藍隊</button>
+            <button type="button" className="btn btn-info m-2" onClick={drawOneToRed}>抽一人入紅隊</button>
         </React.Fragment>
     );
 }
